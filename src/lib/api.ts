4937451cf424602ea338846da692c1e34a1f7a48
yashrevdiwala-bizinsights <yash.bizinsights@gmail.com>
2025-03-20 18:02:26 +0530

    Bugs Fixed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,6 @@ import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
